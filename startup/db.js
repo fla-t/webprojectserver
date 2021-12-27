5352b52bce5c4f882015aa7788410f2b0dcb9192
Salman <61201330+f-lat@users.noreply.github.com>
2021-12-27 11:34:28 +0500

    adjusted to the project req

--- startup/db.js
+++ startup/db.js
@@ -2,6 +2,6 @@ const mongoose = require("mongoose");
 
 module.exports = function () {
     mongoose
-        .connect("mongodb://localhost:27017/assignment5")
+        .connect("mongodb://localhost:27017/project")
         .then(() => console.info("connected to mongoDB"));
 };
